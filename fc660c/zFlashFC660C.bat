426780cbe63c9e635de928bb46bda3fc50c837a5
AndrewBDavis <39595668+AndrewBDavis@users.noreply.github.com>
2018-06-09 22:48:04 -0400

    Update zFlashFC660C.bat

--- fc660c/zFlashFC660C.bat
+++ fc660c/zFlashFC660C.bat
@@ -1,7 +1,7 @@
-cd C:\TMK\dfu-programmer-win-0.7.2 
+cd C:\tmk_keyboard\dfu-programmer-win-0.7.2 
 
 dfu-programmer.exe atmega32u4 erase --force
 
-dfu-programmer.exe atmega32u4 flash C:\TMK\keyboard\fc660c\fc660c.hex
+dfu-programmer.exe atmega32u4 flash C:\tmk_keyboard\keyboard\fc660c\fc660c.hex
 
 dfu-programmer.exe atmega32u4 reset
