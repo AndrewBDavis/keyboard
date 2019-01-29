6274383f06668a4f4dc49b0982174c3d32a76cb0
AndrewBDavis <39595668+AndrewBDavis@users.noreply.github.com>
2019-01-29 00:55:48 -0500

    Add files via upload

--- atreus/keymap.c
+++ atreus/keymap.c
@@ -26,7 +26,7 @@ KEYMAP(
 	/* layer 1 */	
 KEYMAP(	
 	GRV,     7,    8,    9,    0,				  LBRC, RBRC,   UP, TRNS,  DEL, \
-	TRNS,    4,    5,    6, PRNT,	 			  HOME, LEFT, DOWN, RGHT, PGUP, \
+	TRNS,    4,    5,    6, PSCR,	 			  HOME, LEFT, DOWN, RGHT, PGUP, \
 	TRNS,    1,    2,    3, SLCK, 			   END,  INS, TRNS, TRNS, PGDN, \
 	FN2,  CAPS, TRNS, TRNS, TRNS, TRNS,  	 LALT, TRNS, TRNS,  EQL, TRNS, TRNS),
 	
