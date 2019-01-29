e54c243ad33da784aa2ec6b27142635e2ffe585a
AndrewBDavis <39595668+AndrewBDavis@users.noreply.github.com>
2019-01-29 00:48:42 -0500

    Add files via upload

--- atreus/keymap.c
+++ atreus/keymap.c
@@ -1,13 +1,13 @@
 #include "keymap_common.h"
-
 /*
 cd /mnt/c/tmk_keyboard/keyboard/atreus
 make
 sudo avrdude -p atmega32u4 -c avr109 -U flash:w:atreus.hex -P /dev/ttyS4
-make clean
-/dev/ttyS4 refers to COMM Port 4, only applicable if using Bash Windows Subsystem for Linux
-*/
 
+/dev/ttyS4 refers to COMM Port 4, only applicable if using Bash Windows Subsystem for Linux
+
+make clean
+*/
 const action_t PROGMEM fn_actions[] = {
 	[1] = ACTION_LAYER_MOMENTARY(1),  // to Fn overlay
 	[2] = ACTION_LAYER_MOMENTARY(2),  // switch to layer 2
@@ -21,21 +21,21 @@ KEYMAP(
 	Q,      W,    E,    R,    T,					Y,    U,    I,    O,    P, \
 	A,      S,    D,    F,    G,	 				H,    J,    K,    L,  ENT, \
 	Z,      X,    C,    V,    B, 					N,    M, COMM,  DOT, SLSH, \
-	ESC,  TAB, MINS, LGUI, BSPC, LCTL,  	 LSFT,  SPC,  FN1, SCLN, QUOT, BSLS),
+	ESC,  TAB, MINS, LSFT, BSPC, LCTL,  	 LGUI,  SPC,  FN1, SCLN, QUOT, BSLS),
 	
 	/* layer 1 */	
 KEYMAP(	
-	GRV,     W,   UP, LBRC, RBRC,					Y,    7,    8,    9,    0, \
-	A,    LEFT, DOWN, RGHT,    G,	 				H,    4,    5,    6, PGUP, \
-	Z,       X,    C,    V,    B, 				N,    1,    2,    3, PGDN, \
-	TRNS, CAPS, TRNS,  FN2, RSFT, TRNS,  	 TRNS, TRNS, TRNS,  EQL, TRNS, TRNS),
+	GRV,     7,    8,    9,    0,				  LBRC, RBRC,   UP, TRNS,  DEL, \
+	TRNS,    4,    5,    6, TRNS,	 			  HOME, LEFT, DOWN, RGHT, PGUP, \
+	TRNS,    1,    2,    3,  INS, 			   END, TRNS, TRNS, TRNS, PGDN, \
+	FN2,  CAPS, TRNS, TRNS, TRNS, TRNS,  	 LALT, TRNS, TRNS,  EQL, TRNS, TRNS),
 	
 	/* layer 2 */	
 KEYMAP(	
-	TRNS, TRNS, TRNS, TRNS, TRNS,					  F9,   F10,  F11,  F12, TRNS, \
-	TRNS, TRNS, TRNS,  FN9, TRNS,					  F5,    F6,   F7,   F8, TRNS, \
-	TRNS, TRNS, TRNS, TRNS, TRNS,					  F1,    F2,   F3,   F4, TRNS, \
-	TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,	    TRNS, TRNS, TRNS, TRNS, TRNS, TRNS),
+	TRNS, TRNS, TRNS, TRNS, TRNS,					  F9,  F10,  F11,  F12, TRNS, \
+	TRNS, TRNS, TRNS,  FN9, TRNS,					  F5,   F6,   F7,   F8, TRNS, \
+	TRNS, TRNS, TRNS, TRNS, TRNS,					  F1,   F2,   F3,   F4, TRNS, \
+	TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,	    LALT, TRNS, TRNS, TRNS, TRNS, TRNS),
 	
 	/* layer 3 */	
 KEYMAP(	
