d133872e1da3de42793f17e241a61d2c5ce4d424
AndrewBDavis <39595668+AndrewBDavis@users.noreply.github.com>
2019-02-22 00:32:57 -0500

    Add files via upload

--- copam/keymap.c
+++ copam/keymap.c
@@ -1,16 +1,18 @@
 #include "keymap_common.h"
 
 /*
+keycode BTLD will put it in bootloader mode
+
 cd /mnt/c/tmk_keyboard/keyboard/copam
 make clean
 make
 */
 
 const action_t PROGMEM fn_actions[] = {
-	[1] = ACTION_LAYER_TAP_KEY(1, KC_LEFT),
+	[1] = ACTION_LAYER_TAP_KEY(1, KC_LEFT), //The FN1 key acts as the LEFT arrow key when tapped but accesses layer 1 when held
 	[2] = ACTION_LAYER_TOGGLE(2), //Pertains to FN2 - Set up so it acts similar to num lock except with T-nav
 	[3] = ACTION_LAYER_MOMENTARY(3) 
-	
+
 	
 };
 
@@ -23,6 +25,7 @@ KEYMAP(
 	INS, PGUP,	LCTL,   A,   S,   D,   F,   G,   H,   J,    K,    L, SCLN, QUOT,		 	ENT,		LEFT, DOWN, RGHT, PPLS,\
 	HOME,PGDN,	LSFT,   Z,   X,   C,   V,   B,   N,   M, COMM,  DOT, SLSH,				RSFT,	END,  DOWN, PGDN,	   \
 	END,	 DEL,	LGUI, LALT,				SPC,					  FN1, DOWN,   UP, 	RGHT,   	INS,         DEL, PENT),
+	
 	/* layer 1 */
 KEYMAP(
 	TRNS, TRNS,  GRV,   F1,   F2,   F3,   F4,   F5,   F6,   F7,   F8,   F9,  F10,  F11,  F12, TRNS, TRNS, 	NLCK, SLCK, PAUS, TRNS, \
@@ -30,6 +33,7 @@ KEYMAP(
 	TRNS, TRNS,   TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, HOME, LEFT, RGHT,		 TRNS, 	TRNS, TRNS, TRNS, TRNS, \
 	TRNS, TRNS,    LSFT, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,  END, DOWN,			 RSFT, 	TRNS, TRNS, TRNS,       \
 	TRNS, TRNS, TRNS, TRNS,                   TRNS,                               TRNS, TRNS, TRNS, TRNS, 	TRNS,       TRNS, TRNS  ),
+	
 	/* layer 2 */
 KEYMAP(
 	TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, 	TRNS, TRNS, TRNS, TRNS, \
@@ -37,7 +41,8 @@ KEYMAP(
 	TRNS, TRNS,   TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,           TRNS, 	P4,	   P5,   P6, TRNS, \
 	TRNS, TRNS,     TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,               TRNS, 	P1,     P2,   P3,       \
 	TRNS, TRNS, TRNS, TRNS,                   TRNS,                               TRNS, TRNS, TRNS, TRNS, 	P0,		  PDOT, TRNS  ),
-	/* layer 3 */
+	
+	/* layer 3 (unused) */ 
 KEYMAP(
 	F5,  PSCR, 	ESC,   1,   2,   3,   4,   5,   6,   7,   8,   9,   0, MINS,  EQL,  GRV,	BSLS,	DEL,	PSLS, PAST, EQL, \
 	SLCK, FN2,	TAB,    Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P, LBRC, RBRC,		BSPC,	P7,  P8,   P9,	  PMNS,\
