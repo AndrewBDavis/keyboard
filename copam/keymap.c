7a22d9acf1443978103111e026d74a9d71aec6d8
Andrew B. Davis <39595668+AndrewBDavis@users.noreply.github.com>
2020-10-02 16:50:21 -0400

    Update keymap.c

--- copam/keymap.c
+++ copam/keymap.c
@@ -18,11 +18,11 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	
 	/* layer 0 (Base Layer) */	
 KEYMAP(
-	F5,  PSCR, 	ESC,   1,   2,   3,   4,   5,   6,   7,   8,   9,   0, MINS,  EQL,  GRV,  BSLS,   DEL, PSLS, PAST,  EQL, \
-	SLCK, FN2,	TAB,    Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P, LBRC, RBRC,	  BSPC,  HOME,   UP, PGUP, PMNS, \
-	INS, PGUP,	LCTL,   A,   S,   D,   F,   G,   H,   J,    K,    L, SCLN, QUOT,	   ENT,	 LEFT, DOWN, RGHT, PPLS, \
-	HOME,PGDN,	LSFT,   Z,   X,   C,   V,   B,   N,   M, COMM,  DOT, SLSH,			 RSFT,	END, DOWN, PGDN, \
-	END,  DEL,	LGUI, LALT,			SPC,			 FN1, DOWN,   UP, RGHT,   INS,        DEL, PENT  ),
+	F5,  PSCR, 	ESC,   1,   2,   3,   4,   5,   6,   7,   8,   9,   0, MINS,  EQL,  GRV,  	BSLS,   DEL, PSLS, PAST,  EQL, \
+	SLCK, FN2,	TAB,    Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P, LBRC, RBRC,	  	BSPC,  HOME,   UP, PGUP, PMNS, \
+	INS, PGUP,	LCTL,   A,   S,   D,   F,   G,   H,   J,    K,    L, SCLN, QUOT,	   	 ENT,  LEFT, DOWN, RGHT, PPLS, \
+	HOME,PGDN,	LSFT,   Z,   X,   C,   V,   B,   N,   M, COMM,  DOT, SLSH,		  	RSFT,	END, DOWN, PGDN,       \
+	END,  DEL,	LGUI, LALT,			SPC,			       FN1, DOWN,   UP, RGHT,   INS,        DEL, PENT  ),
 	
 	/* layer 1 (function layer) */
 KEYMAP(
