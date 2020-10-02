cea2b3bc20aed369e41d40a4c0b6647dd99de371
Andrew B. Davis <39595668+AndrewBDavis@users.noreply.github.com>
2020-10-02 16:45:49 -0400

    Update keymap.c

--- copam/keymap.c
+++ copam/keymap.c
@@ -18,19 +18,19 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	
 	/* layer 0 (Base Layer) */	
 KEYMAP(
-	F5,  PSCR, 	ESC,   1,   2,   3,   4,   5,   6,   7,   8,   9,   0, MINS,  EQL,  GRV,	BSLS,	DEL,	 PSLS, PAST, EQL, \
-	SLCK, FN2,	TAB,    Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P, LBRC, RBRC,		BSPC,	HOME,   UP, PGUP, PMNS,\
-	INS, PGUP,	LCTL,   A,   S,   D,   F,   G,   H,   J,    K,    L, SCLN, QUOT,		 	ENT,	LEFT, DOWN, RGHT, PPLS,\
-	HOME,PGDN,	LSFT,   Z,   X,   C,   V,   B,   N,   M, COMM,  DOT, SLSH,				RSFT,	END,  DOWN, PGDN,	   \
-	END,  DEL,	LGUI, LALT,				SPC,					  FN1, DOWN,   UP, 	RGHT,   	INS,         DEL, PENT),
+	F5,  PSCR, 	ESC,   1,   2,   3,   4,   5,   6,   7,   8,   9,   0, MINS,  EQL,  GRV,  BSLS,   DEL, PSLS, PAST,  EQL, \
+	SLCK, FN2,	TAB,    Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P, LBRC, RBRC,	  BSPC,  HOME,   UP, PGUP, PMNS, \
+	INS, PGUP,	LCTL,   A,   S,   D,   F,   G,   H,   J,    K,    L, SCLN, QUOT,	   ENT,	 LEFT, DOWN, RGHT, PPLS, \
+	HOME,PGDN,	LSFT,   Z,   X,   C,   V,   B,   N,   M, COMM,  DOT, SLSH,			 RSFT,	END, DOWN, PGDN, \
+	END,  DEL,	LGUI, LALT,			SPC,			 FN1, DOWN,   UP, RGHT,   INS,        DEL, PENT  ),
 	
 	/* layer 1 (function layer) */
 KEYMAP(
-	TRNS, TRNS,  GRV,   F1,   F2,   F3,   F4,   F5,   F6,   F7,   F8,   F9,  F10,  F11,  F12, TRNS, TRNS, 	NLCK, SLCK, PAUS, TRNS, \
+	TRNS, TRNS,  GRV,   F1,   F2,   F3,   F4,   F5,   F6,   F7,   F8,   F9,  F10,  F11,  F12, TRNS,  TRNS, 	NLCK, SLCK, PAUS, TRNS, \
 	TRNS, TRNS,  CAPS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,  INS, SLCK, PSCR,   UP, PGDN,	 PGUP, 	TRNS, TRNS, TRNS, PSCR, \
 	TRNS, TRNS,   TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, HOME, LEFT, RGHT,		 TRNS, 	TRNS, TRNS, TRNS, TRNS, \
 	TRNS, TRNS,    LSFT, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,  END, DOWN,		 RSFT, 	TRNS, TRNS, TRNS,       \
-	TRNS, TRNS, TRNS, TRNS,                   TRNS,                               TRNS, TRNS, TRNS, TRNS, 	TRNS,       TRNS, TRNS  ),
+	TRNS, TRNS, TRNS, TRNS,                   TRNS,                                TRNS, TRNS, TRNS, TRNS, 	TRNS,       TRNS, TRNS  ),
 	
 	/* layer 2 (numlock layer) */
 KEYMAP(
