2374b7ffad7f9fbb8d05160ea9d3b26ecfbf25dc
Andrew B. Davis <39595668+AndrewBDavis@users.noreply.github.com>
2020-10-02 16:42:24 -0400

    Update keymap.c

--- copam/keymap.c
+++ copam/keymap.c
@@ -20,33 +20,33 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 KEYMAP(
 	F5,  PSCR, 	ESC,   1,   2,   3,   4,   5,   6,   7,   8,   9,   0, MINS,  EQL,  GRV,	BSLS,	DEL,	 PSLS, PAST, EQL, \
 	SLCK, FN2,	TAB,    Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P, LBRC, RBRC,		BSPC,	HOME,   UP, PGUP, PMNS,\
-	INS, PGUP,	LCTL,   A,   S,   D,   F,   G,   H,   J,    K,    L, SCLN, QUOT,		 	ENT,		LEFT, DOWN, RGHT, PPLS,\
+	INS, PGUP,	LCTL,   A,   S,   D,   F,   G,   H,   J,    K,    L, SCLN, QUOT,		 	ENT,	LEFT, DOWN, RGHT, PPLS,\
 	HOME,PGDN,	LSFT,   Z,   X,   C,   V,   B,   N,   M, COMM,  DOT, SLSH,				RSFT,	END,  DOWN, PGDN,	   \
-	END,	 DEL,	LGUI, LALT,				SPC,					  FN1, DOWN,   UP, 	RGHT,   	INS,         DEL, PENT),
+	END,  DEL,	LGUI, LALT,				SPC,					  FN1, DOWN,   UP, 	RGHT,   	INS,         DEL, PENT),
 	
 	/* layer 1 (function layer) */
 KEYMAP(
 	TRNS, TRNS,  GRV,   F1,   F2,   F3,   F4,   F5,   F6,   F7,   F8,   F9,  F10,  F11,  F12, TRNS, TRNS, 	NLCK, SLCK, PAUS, TRNS, \
 	TRNS, TRNS,  CAPS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,  INS, SLCK, PSCR,   UP, PGDN,	 PGUP, 	TRNS, TRNS, TRNS, PSCR, \
 	TRNS, TRNS,   TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, HOME, LEFT, RGHT,		 TRNS, 	TRNS, TRNS, TRNS, TRNS, \
-	TRNS, TRNS,    LSFT, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,  END, DOWN,			 RSFT, 	TRNS, TRNS, TRNS,       \
+	TRNS, TRNS,    LSFT, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,  END, DOWN,		 RSFT, 	TRNS, TRNS, TRNS,       \
 	TRNS, TRNS, TRNS, TRNS,                   TRNS,                               TRNS, TRNS, TRNS, TRNS, 	TRNS,       TRNS, TRNS  ),
 	
 	/* layer 2 (numlock layer) */
 KEYMAP(
 	TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, 	TRNS, TRNS, TRNS, TRNS, \
-	TRNS, TRNS,  TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,      TRNS, 	P7,	   P8,   P9, TRNS, \
-	TRNS, TRNS,   TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,           TRNS, 	P4,	   P5,   P6, TRNS, \
+	TRNS, TRNS,  TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,      TRNS, 	P7,	P8,   P9, TRNS, \
+	TRNS, TRNS,   TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,           TRNS, 	P4,	P5,   P6, TRNS, \
 	TRNS, TRNS,     TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,               TRNS, 	P1,     P2,   P3,       \
-	TRNS, TRNS, TRNS, TRNS,                   TRNS,                               TRNS, TRNS, TRNS, TRNS, 	P0,		  PDOT, TRNS  ),
+	TRNS, TRNS, TRNS, TRNS,                   TRNS,                               TRNS, TRNS, TRNS, TRNS, 	P0,   PDOT, 	  TRNS  ),
 	
 	/* layer 3 (unused) */ 
 KEYMAP(
-	F5,  PSCR, 	ESC,   1,   2,   3,   4,   5,   6,   7,   8,   9,   0, MINS,  EQL,  GRV,	BSLS,	DEL,	PSLS, PAST, EQL, \
-	SLCK, FN2,	TAB,    Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P, LBRC, RBRC,		BSPC,	P7,  P8,   P9,	  PMNS,\
-	INS, PGUP,	LCTL,   A,   S,   D,   F,   G,   H,   J,    K,    L, SCLN, QUOT,		 	ENT,		P4,  P5,   P6,	  PPLS,\
-	HOME,PGDN,	LSFT,   Z,   X,   C,   V,   B,   N,   M, COMM,  DOT, SLSH,				RSFT,	P1,  P2,   P3,		  \
-	END,	 DEL,	LGUI, LALT,				SPC,					  FN1, DOWN,   UP, 	RGHT,   	P0,		 PDOT, PENT),
+	F5,  PSCR, 	ESC,   1,   2,   3,   4,   5,   6,   7,   8,   9,   0, MINS,  EQL,  GRV,	BSLS,	DEL, PSLS, PAST,  EQL, \
+	SLCK, FN2,	TAB,    Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P, LBRC, RBRC,		BSPC,	P7,    P8,   P9, PMNS, \
+	INS, PGUP,	LCTL,   A,   S,   D,   F,   G,   H,   J,    K,    L, SCLN, QUOT,		 ENT,	P4,    P5,   P6, PPLS, \
+	HOME,PGDN,	LSFT,   Z,   X,   C,   V,   B,   N,   M, COMM,  DOT, SLSH,			RSFT,	P1,    P2,   P3,       \
+	END,  DEL,	LGUI, LALT,				SPC,			FN1, DOWN,  UP, RGHT,   P0,  PDOT, 	 PENT  ),
 };
 
 /*
