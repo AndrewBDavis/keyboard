62d9b30081fba8ab69b5c65d37686aee5884bba6
AndrewBDavis <Andrew@AndrewBDavis.com>
2022-03-25 14:22:30 -0400

    Update led.c

--- next76/led.c
+++ next76/led.c
@@ -17,17 +17,17 @@ void led_set(uint8_t usb_led) {
     }
     
 	if (usb_led & (1<<USB_LED_SCROLL_LOCK)) {
-		PORTB |= (1<<7);	// Turn num lock on
+		PORTB |= (1<<5);	// Turn num lock on
     } else {
-		PORTB &= ~(1<<7);	// Turn num lock off
+		PORTB &= ~(1<<5);	// Turn num lock off
     }
 }
 
 void hook_layer_change(uint32_t layer_state) //num lock layer turns on when layer 2 is active
 {
 	if (layer_state & (1L<<2))
-		PORTB |= (1<<5);
+		PORTB |= (1<<7);
 	else 
-		PORTB &= ~(1<<5);
+		PORTB &= ~(1<<7);
 
 }
